d5e2d5bad78893174e52389be0f00ed953f2bcff
Tommaso Piazza <tommaso.piazza@gmail.com>
2018-12-06 14:23:04 +0100

    Fix minimum version check (#10)

--- spec/rome_action_spec.rb
+++ spec/rome_action_spec.rb
@@ -5,5 +5,18 @@ describe Fastlane::Actions::RomeAction do
       expect(Fastlane::UI).to receive(:message).with("#{path_to_rome_binary.chomp} --version")
       Fastlane::Actions::RomeAction.run({:binary_path => path_to_rome_binary, :command => "version"})
     end
+    it 'checks minimum version correctly' do
+      expect(Fastlane::Actions::RomeAction.version_is_greater_or_equal("0.0.0.1", "")).to be true
+      expect(Fastlane::Actions::RomeAction.version_is_greater_or_equal("0.0.0.0", "")).to be true
+      expect(Fastlane::Actions::RomeAction.version_is_greater_or_equal("0", "1")).to be false
+      expect(Fastlane::Actions::RomeAction.version_is_greater_or_equal("0.18.0.51", "0.13.1.15")).to be true
+      expect(Fastlane::Actions::RomeAction.version_is_greater_or_equal("0.13.1.15", "0.18.0.51")).to be false
+      expect(Fastlane::Actions::RomeAction.version_is_greater_or_equal("0.18.0.51", "0")).to be true
+      expect(Fastlane::Actions::RomeAction.version_is_greater_or_equal("0.18.0.51", "0.18.1.51")).to be false
+      expect(Fastlane::Actions::RomeAction.version_is_greater_or_equal("0.18.1.51", "0.18.0.51")).to be true
+      expect(Fastlane::Actions::RomeAction.version_is_greater_or_equal("0.18.0.51", "0.18.1.0")).to be false
+      expect(Fastlane::Actions::RomeAction.version_is_greater_or_equal("0.18.0.51", "0.19.0.49")).to be false
+      expect(Fastlane::Actions::RomeAction.version_is_greater_or_equal("0.19.0.49", "0.18.0.51")).to be true
+    end
   end
 end
